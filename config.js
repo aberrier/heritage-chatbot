932ded4194be2c4a4f28c1d4304dfd93bbf56c53
Alain <alain.berrier@outlook.com>
2017-01-02 06:41:18 +0100

    no message

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {}
+
+config.pageAccessToken = 'EAADkRXXneoABAGKNfSx4Wnwk5KautZAq2hy1Ed85fGNz71mGyZAUI5t4ZB3JGkRCImZAYpVTc6u6YLA5YJKNytsrD7AVQSX0pbWKSw7TNDZCnDVVPX3hun2KDi23ZA89smptvOu7g2PlrWhdhk5MSyatT1gVpkYxhDuAE1yl6NGwZDZD'
+config.validationToken = 'ABCD1234'
+config.recastToken = '839463246e404a1b4fcb50d7c7189472'
+config.language = 'en'
+
+module.exports = config
