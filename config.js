380d1548507a4f547b8f3c63fe1c097a8401f20d
Alain <alain.berrier@outlook.com>
2017-01-02 15:47:44 +0100

    no message

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {}
 
-config.pageAccessToken = 'EAADkRXXneoABAGKNfSx4Wnwk5KautZAq2hy1Ed85fGNz71mGyZAUI5t4ZB3JGkRCImZAYpVTc6u6YLA5YJKNytsrD7AVQSX0pbWKSw7TNDZCnDVVPX3hun2KDi23ZA89smptvOu7g2PlrWhdhk5MSyatT1gVpkYxhDuAE1yl6NGwZDZD'
+config.pageAccessToken = 'EAADkRXXneoABAPIK0Y8kwBYTdtZCz0gOweCuxtcZBI0nBLLrZCTx1LNyglRvyfCrG1c5qM69FCrGAI9s4CT0iywrZCCcAULXDOR3FHRjw7rE3hYZAmYDx6MjusTsbOCpZBJkti71q8k3uvKIyh6pTjPgG4PnNBuH8mAugahXioZBwZDZD'
 config.validationToken = 'ABCD1234'
 config.recastToken = '839463246e404a1b4fcb50d7c7189472'
 config.language = 'en'
